8f01bac928f25c37ad45e015e6445604fcb046ff
Elizeu <Elizeu@192.168.0.3>
2017-04-20 00:38:44 -0300

    teste de .jar para o heroku

--- src/main/java/configuracao/Application.java
+++ src/main/java/configuracao/Application.java
@@ -1,14 +1,11 @@
 package configuracao;
 
-import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 @Configuration
