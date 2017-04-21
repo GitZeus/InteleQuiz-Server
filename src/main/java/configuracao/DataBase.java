110083731e871839430ace41925ebbcd3c0c6361
Elizeu <Elizeu@192.168.0.3>
2017-04-21 01:53:31 -0300

    Ajustes no projeto para o banco de dados do heroku

--- src/main/java/configuracao/DataBase.java
+++ src/main/java/configuracao/DataBase.java
@@ -52,9 +52,9 @@ public class DataBase {
         try {
             BasicDataSource dataSource = new BasicDataSource();
             dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-            dataSource.setUrl("jdbc:" + System.getenv("CLEARDB_DATABASE_URL"));
-//            dataSource.setUsername("root");
-//            dataSource.setPassword("root");
+            dataSource.setUrl(System.getenv("CLEARDB_URL"));
+            dataSource.setUsername(System.getenv("CLEARDB_USER"));
+            dataSource.setPassword(System.getenv("CLEARDB_PASS"));
 //            dataSource.setUrl("jdbc:mysql://localhost:3306/intelequiz?autoReconnect=true&useSSL=false");
 //            dataSource.setUsername("root");
 //            dataSource.setPassword("root");
